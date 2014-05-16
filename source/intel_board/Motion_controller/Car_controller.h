b6d7b0882808ce1d9e4325a8aff10b4e3a64c510
Edward <huangxx_2155@qq.com>
2014-05-16 12:22:12 +0800

    helloworld for the cross compilation

--- source/intel_board/Motion_controller/Car_controller.h
+++ source/intel_board/Motion_controller/Car_controller.h
@@ -1,5 +1,11 @@
 /*
 */
+struct Car_controll_msg
+{
+	uint32_t x; //forward, centimeter
+	uint32_t y; //right-hand side, centimeter
+	uint32_t z; //up, centimeter
+};
 class Car_controller
 {
 private:
