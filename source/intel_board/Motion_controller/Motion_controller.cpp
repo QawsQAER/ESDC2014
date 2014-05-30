e6acadb72bc7a48efea780b9506559b13b7446fe
Edward <huangxx_2155@qq.com>
2014-05-30 17:26:54 +0800

    1. create a Makefile for Motion_controller, so that it can be compiled (But not yet integrated into the whole system)

---
+++ source/intel_board/Motion_controller/Motion_controller.cpp
@@ -0,0 +1,6 @@
+#include "Motion_controller.h"
+
+Motion_controller::Motion_controller()
+{
+	
+}
--- source/intel_board/Motion_controller/Motion_controller.h
+++ source/intel_board/Motion_controller/Motion_controller.h
@@ -5,14 +5,20 @@
 #ifndef _MOTION_CONTROLLER_H
 #define _MOTION_CONTROLLER_H
 
-#include "Car_controller.h"
+//for queue
+#include <queue>
+//for standard int type
+#include <stdint.h>
 #include "Controller_Com.h"
+#include <opencv2/opencv.hpp>
+#include "../lib/message.h"
 
 class Motion_controller
 {
 private:
-	static queue cmd_queue;
-	static Controller_Com Com;
+	cv::Rect ref;
+	std::queue<struct IntelCarCmd> cmd_queue;
+	Controller_Com *Com;
 public:
 	Motion_controller();
 	~Motion_controller();
