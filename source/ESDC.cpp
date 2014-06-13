2c61128cd143330ee365d089d50a3bd1778f8587
Edward <huangxx_2155@qq.com>
2014-06-13 12:10:31 +0800

    1. restructure ESDC.cpp 2. add more dependency files in Makefile

--- source/ESDC.cpp
+++ source/ESDC.cpp
@@ -46,56 +46,57 @@ intel_board *robot;
 
 int main(int argc, char ** argv) 
 {
-        /* prints Hello World */
-        cout << "Hello Intel ESDC" << endl; 
-        //mode default value MANUAL MODE
-        uint8_t mode = 1;
-        //img_source default using Cellphone
-        uint8_t img_source = 0;
-        generate_dir();
-        signal(SIGTERM,exit_routine);
-        signal(SIGINT,exit_routine);
-        if(argc >= 2)
-        {
-        	//the user has set the mode
-        	mode = atoi(argv[1]);
-                //if the mode is image processing mode
-                switch(mode)    
-                {
-                        case 0://case for auto mode
-                                printf("The robot is going to initiate in default mode\n");
-                        break;
-                        
-                        case 1://case for manual mode
-                                //if the user has specify the img_source
-                                if(argc >= 3)
-                                        img_source = atoi(argv[2]);
+	/* prints Hello World */
+	cout << "Hello Intel ESDC" << endl;
+	/*
+	   mode, default value MANUAL MODE
+	   img_source, default value using Cellphone 
+	*/
+	uint8_t mode = 1;
+	uint8_t img_source = 0;
+	PATH_TEMP = (char *) malloc(sizeof(char) * FILENAME_LENGTH);
+	generate_dir();
+	signal(SIGTERM,exit_routine);
+	signal(SIGINT,exit_routine);
+	if(argc >= 2)//the user has set the mode
+	{
+		mode = atoi(argv[1]);
+		switch(mode)
+		{
+			case 0://case for auto mode
+				printf("The robot is going to initiate in default mode\n");
+			break;
+	
+			case 1://case for manual mode
+				if(argc >= 3)//if the user has specify the img_source
+					img_source = atoi(argv[2]);
+			break;
 
-                        break;
-
-                        case 2://case for image processing mode
-                                if(argc != 3)
-                                {
-                                        printf("Please specify the directory for image processing\n");
-                                        exit(-1);
-                                }
-                                else
-                                {
-                                        dir_path = (char *) malloc(sizeof(char) * FILENAME_LENGTH);
-                                        strcpy(dir_path,argv[2]);
-                                }
-                        break;
-                }
+			case 2://case for image processing mode
+				if(argc != 3)
+				{
+					printf("Please specify the directory for image processing\n");
+					exit(-1);
+				}
+				else
+				{
+					dir_path = (char *) malloc(sizeof(char) * FILENAME_LENGTH);
+					strcpy(dir_path,argv[2]);
+				}
+				break;
+			default://a undefined mode
+				printf("Invalid mode specified by the user\n");
+				exit(-1);
+			break; 
         }
-        else
-                printf("The robot is going to initiate in default mode\n");
+    }
+    else // the user has not set the mode
+    	printf("The robot is going to initiate in default mode\n");
 
-
-        robot = new intel_board(mode,img_source);
-        robot->main_function();
-
-        exit_routine(0);
-        return 0;
+    robot = new intel_board(mode,img_source);
+    robot->main_function();
+	exit_routine(0);
+	return 0;
 }
 
 void exit_routine(int arg)
@@ -109,27 +110,25 @@ void exit_routine(int arg)
 
 void generate_dir()
 {
-        printf("generate_dir running\n");
-        PATH_TEMP = (char *) malloc(sizeof(char) * FILENAME_LENGTH);
-        char *filename =(char *) malloc(sizeof(char) * FILENAME_LENGTH);
-        time_t timestamp = time(NULL);
-        struct tm *current_time = gmtime(&timestamp);
-        //get the filename in format of month-day_hour:minute:second
-        sprintf(filename,"%d_%d_%d_%d_%d\0",
-                        current_time->tm_mon,
-                        current_time->tm_mday,
-                        current_time->tm_hour,
-                        current_time->tm_min,
-                        current_time->tm_sec);
-        strcpy(PATH_TEMP,filename);
-        free(filename);
-
-        printf("creating dir named as %s\n",PATH_TEMP);
-        if(mkdir(PATH_TEMP,S_IRWXU) < 0)
-        {
-                printf("fail to create dir %s\n",PATH_TEMP);
-                exit(0);
-        }
-
-        strcat(PATH_TEMP,"/");
+	printf("generate_dir running\n");
+	
+	char *filename =(char *) malloc(sizeof(char) * FILENAME_LENGTH);
+	time_t timestamp = time(NULL);
+	struct tm *current_time = gmtime(&timestamp);
+	//get the filename in format of month-day_hour:minute:second
+	sprintf(filename,"%d_%d_%d_%d_%d\0",
+		current_time->tm_mon,
+		current_time->tm_mday,
+		current_time->tm_hour,
+		current_time->tm_min,
+		current_time->tm_sec);
+	strcpy(PATH_TEMP,filename);
+	free(filename);
+	printf("creating dir named as %s\n",PATH_TEMP);
+	if(mkdir(PATH_TEMP,S_IRWXU) < 0)
+	{
+		printf("fail to create dir %s\n",PATH_TEMP);
+		exit(0);
+	}
+	strcat(PATH_TEMP,"/");
 }
